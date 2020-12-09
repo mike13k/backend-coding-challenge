9b921b1653234e36c75fe53b3c48dcaeff056e5d
Michael Khalil <mikegeorge2012@gmail.com>
2020-12-09 04:41:44 +0200

    add lang_ns to api

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask_restplus import Api
 from flask import Blueprint
 
+from .main.controller.lang_controller import api as lang_ns
 
 blueprint = Blueprint('api', __name__)
 
 api = Api(blueprint)
+
+api.add_namespace(lang_ns, path='/lang')
