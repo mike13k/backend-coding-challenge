e5fcae9ff4450af1f765f638401373f307ec578c
Michael Khalil <mikegeorge2012@gmail.com>
2020-12-09 04:32:11 +0200

    create manager.py

---
+++ app/manage.py
@@ -0,0 +1,20 @@
+import os
+
+from flask_script import Manager
+
+from app.main import create_app
+from app import blueprint
+
+app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+app.register_blueprint(blueprint)
+
+app.app_context().push()
+
+manager = Manager(app)
+
+@manager.command
+def run():
+    app.run()
+
+if __name__ == '__main__':
+    manager.run()
